10675ca94e2e8880813705be5353a225ebe24a0f
Khadim Fall <khadimfall0602@gmail.com>
2016-05-08 18:29:57 +0200

    setup fix
    setup is working now

--- api/v1/setup/index.php
+++ api/v1/setup/index.php
@@ -5,13 +5,15 @@ require '../config.php';
 $link = openCON();
 if(!$link){die('Connection ERROR!');}
 
-$db_selected = mysqli_select_db('foo', $link);
+$db_selected = mysqli_select_db($link, $config['db']['db']);
 if (!$db_selected) {
   // START SETUP
   if(file_exists('setup.sql')){
-    $link->query('CREATE DATABASE IF NOT EXISTS '+$config['db']['db']+';')
-    if(mysqli_select_db('foo', $link)){die('Cant create Database!');}
-    $link->query(file_get_contents('setup.sql'));
+    $link->query('CREATE DATABASE IF NOT EXISTS '.$config['db']['db'].';');
+    if($link->error){die($link->error);}
+    if(!mysqli_select_db($link, $config['db']['db'])){die('Cant create Database!');}
+    $link->multi_query(file_get_contents('setup.sql'));
+    if($link->error){die($link->error);}
     echo "Finished!";
   }else{
     die('setup.sql not found!');
