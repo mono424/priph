9247501304335e9b19afa3dd0b9cb480e661c369
Khadim Fall <khadimfall0602@gmail.com>
2016-05-13 02:34:58 +0200

    Gerneral Design

--- share.php
+++ share.php
@@ -1,43 +1,91 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <!-- TITLE & CHARSET -->
-    <meta charset="utf-8">
-    <title>Priph - </title>
+<head>
+  <!-- TITLE & CHARSET -->
+  <meta charset="utf-8">
+  <title>Priph - </title>
 
-    <!-- FAVICON -->
-    <link rel="shortcut icon" type="image/ico" href="img/priph.ico" />
+  <!-- FAVICON -->
+  <link rel="shortcut icon" type="image/ico" href="img/priph.ico" />
 
-    <!-- SHARE STYLESHEET -->
-    <link rel="stylesheet" href="/css/share.css">
+  <!-- SHARE STYLESHEET -->
+  <link rel="stylesheet" href="/css/share.css">
 
-    <!-- MOBILE ;) -->
-    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width">
-  </head>
-  <body>
-    <div class="topbar">
+  <!-- MOBILE ;) -->
+  <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width">
 
+  <!-- PRIPH JS API -->
+  <!-- <script type="text/javascript" src="http://priph.com/api/v1/js/script.min.js"></script> -->
+  <script type="text/javascript" src="api/v1/js/script.local.js"></script>
+
+  <!--  -->
+</head>
+<body>
+  <!-- <div class="topbar"></div> -->
+  <center><a href="/"><img class="logo" src="img/priph_full.png" alt="" /></a></center>
+  <div class="wrapper">
+
+    <!-- IMAGE CONTENT -->
+    <div class="image">
+      <img src="https://placebear.com/g/400/500" alt="" />
     </div>
-    <div class="wrapper">
 
-      <!-- IMAGE CONTENT -->
-      <div class="image">
-        <img src="https://placebear.com/g/400/500" alt="" />
+    <!-- INFO CONTENT -->
+    <div class="info">
+      <!-- AUTHOR STUFF -->
+      <div class="author cf">
+        <img src="https://placebear.com/g/50/50" alt="" />
+        <div>Khadim Fall</div>
       </div>
+      <hr>
 
-      <!-- INFO CONTENT -->
-      <div class="info">
-        <!-- AUTHOR STUFF -->
-        <div class="author cf">
-          <img src="api/v1/?action=getProfilePicture&width=58&height=58" alt="" />
-          <div>Khadim Fall</div>
+      <!-- COMMENTBOX -->
+      <div class="comment-hl">Comments</div>
+      <div class="commentbox">
+
+        <!-- A COMMENT -->
+        <div class="comment">
+
+          <!-- COMMENT AUTHOR -->
+          <div class="comment-author cf">
+            <img src="https://placebear.com/g/20/20" alt="" />
+            <div>Khadim Fall</div>
+          </div>
+
+          <!-- COMMENT CONTENT -->
+          <div class="content">
+            <p>
+              Das ist ein standart Kommentar in Deutsch :OO Das ist ein standart Kommentar in Deutsch :OO Und der ist auch noch ziemlich lang
+            </p>
+          </div>
         </div>
-        <hr>
-        <div class="commentbox">
-          <p>Comments</p>
+
+        <!-- A COMMENT -->
+        <div class="comment">
+
+          <!-- COMMENT AUTHOR -->
+          <div class="comment-author cf">
+            <img src="https://placebear.com/g/20/20" alt="" />
+            <div>Khadim Fall</div>
+          </div>
+
+          <!-- COMMENT CONTENT -->
+          <div class="content">
+            <p>
+              Das ist ein standart Kommentar in Deutsch :OO Das ist ein standart Kommentar in Deutsch :OO Und der ist auch noch ziemlich lang
+            </p>
+          </div>
+        </div>
+
+      </div>
+      <div class="comment-form">
+        <div class="commentwrap">
+          <input type="text" name="comment" placeholder="Write something nice..">
+          <img src="https://placebear.com/g/20/20" alt="" />
         </div>
       </div>
-
     </div>
-  </body>
+
+  </div>
+</body>
 </html>
